f3ba3f951fc3ed8184ca1b01e12071458c221968
Michael Tipton <metipton@ncsu.edu>
2019-04-06 17:37:36 -0400

    order review stuff

--- StarForge/star-forge/src/containers/Checkout/CheckoutForm/OrderReviewForm/ItemDescriptionCard/ItemDescriptionCard.js
+++ StarForge/star-forge/src/containers/Checkout/CheckoutForm/OrderReviewForm/ItemDescriptionCard/ItemDescriptionCard.js
@@ -3,11 +3,17 @@ import classes from './ItemDescriptionCard.css';
 
 const ItemDescriptionCard = (props) => (
     <div className={classes.orderItemContainer}>
-        <img className={classes.Image} src={props.image} alt="Item_image"/>
+        <div className={classes.ImageContainer}>
+            <div className={classes.ImageContainer2}>
+                <img className={classes.Image} src={props.image} alt="Item_image"/>
+            </div>
+        </div>
         <div className={classes.Info}>
-            <p className={classes.Name}>{props.name}</p>
-            <p>{props.description}</p>
-            <p>{props.material}</p>
+            <div className={classes.InfoContainer}>
+                <p className={classes.Name}>{props.name}</p>
+                <p className={classes.Para}>{props.description}</p>
+                <p className={classes.Para}>{props.material}</p>
+            </div>
         </div>
         <div className={classes.Price}>
             ${props.price}
